baac2e8364fa759e0a48e9785df0adfec4fcf9f2
343079463 <343079463@C-FTFYJB2.tdsb.on.ca>
2026-06-10 10:15:44 -0400

    Added comments, created more stages to move further within the story, fixed existing issues

--- src/the/magic/bed/MySketch.java
+++ src/the/magic/bed/MySketch.java
@@ -18,8 +18,8 @@ public class MySketch extends PApplet {
     private Person person1;
     String userInput = "";
     double stage = 0;
-    PImage bg1Image, bg2Image, bg3Image; // background images
-    PImage dialog1, dialog2; // dialogues
+    PImage bg1Image, bg2Image, bg3Image, bg4Image; // background images
+    PImage dialog1, dialog2, dialog3, dialog4; // dialogues
     
     // the main characters facial perspectives
     String pfp1 = "images/1.png";
@@ -49,6 +49,8 @@ public class MySketch extends PApplet {
         bg2Image.resize(1000, 700);
         bg3Image = loadImage("images/bgimage2.png");
         bg3Image.resize(1000, 700);
+        bg4Image = loadImage("images/bgImage3.png");
+        bg4Image.resize(1000, 700);
         
         // characters
         person1 = new Person (this, 450, 500, "Prince", pfp1);
@@ -60,20 +62,22 @@ public class MySketch extends PApplet {
         //dialogues
         dialog1 = loadImage("images/dialog1.png");
         dialog2 = loadImage("images/dialog2.png");
+        dialog3 = loadImage("images/dialog3.png");
+        dialog4 = loadImage("images/dialog4.png");
     }
     
     
     public void draw() {
         background(255, 255, 255); 
 
-        if (stage == 0) {
-            background(bg1Image); 
-            textSize(20);
-            text("Welcome!", 450, 350);
-            textSize(15);
-            text("Enter name: ", 450, 365);
-            text(userInput, 450, 380);
-            /** NOT WORKING, FIX!!
+        if (stage == 0) { // if we are on stage 0 (intro)
+            background(bg1Image); // set the background
+            textSize(20); // make the text size 20
+            text("Welcome!", 450, 350); // create a welcome message
+            textSize(15); // set the text size as 15
+            text("Enter name: ", 450, 365); // create a space for the user to enter their name
+            text(userInput, 450, 380); // gett eh user input
+            String username = userInput;
             try { // try the code below
                 // Open the file and add to the end instead of deleting previous text
                 FileWriter w = new FileWriter("usernames", true); // create the file writer for the verdict.txt file
@@ -85,27 +89,28 @@ public class MySketch extends PApplet {
             } catch (IOException e) { // catch the io exception if there is one
                 System.out.println("Invalid Username"); // Error message
             } // close the try-catch
-            */
-        } else if (stage == 1) {
-            background(bg2Image); 
-            mango1.draw();
-            person1.draw();
-        } else if (stage == 2){
-            background(bg3Image);
-            fill(255, 255, 255);
-            this.text("AAAHH PLEASE DONT EAT ME", antKing.x, antKing.y - 45);
-            antKing.draw();
-        } else if (stage == 3){
-            background(bg2Image); 
-            mango1.draw();
-            antKing.draw();
-            person1.draw();
-        } else if (stage == 3.1){ // if we are on stage 3.1
+        } else if (stage == 1) { // if we are on stage 1
+            background(bg2Image);  // set the background
+            mango1.draw(); // draw the mango
+            person1.draw(); // draw the price
+        } else if (stage == 2){ // if we are on stage 2
+            background(bg3Image); // set the background
+            fill(255, 255, 255); // set the text color to white
+            this.text("AAAHH PLEASE DONT EAT ME", antKing.x, antKing.y - 45); // set the text for the ant king above him
+            antKing.draw(); // draw the ant king
+        } else if (stage == 3){ // if we are on stage 3
+            background(bg2Image);  // set the background 
+            mango1.draw(); // draw the mango
+            antKing.draw(); // draw the ant king
+            person1.draw(); // draw the prince
+        } else if (stage == 3.1 || stage == 3.2 || stage == 3.4){ // if we are on stage 3.1, 3.2,or 3.4
             background(bg2Image); // set the background
             mango1.draw(); // draw the mango
-            antKing.draw(); // draw the ant king 
+            antKing.draw(); // draw the ant king
             person1.draw(); // draw the prince
-        }
+        } else if (stage == 3.3){
+            background(bg4Image);
+        } 
         
         if (keyPressed) {
             if (keyCode == LEFT) {
@@ -123,7 +128,8 @@ public class MySketch extends PApplet {
             }
         }
 
-
+        //Collision detection and Stage changes
+        // dialogue changes as well
         if (stage == 1 && person1.isCollidingWith(mango1)) {
             stage = 2;
         }
@@ -134,6 +140,14 @@ public class MySketch extends PApplet {
         
         if (stage == 3.1 && person1.isCollidingWith(antKing)){
             image(dialog2, 200, 500);
+        }
+        
+        if (stage == 3.2 && person1.isCollidingWith(antKing)){
+            image(dialog3, 200, 500);
+        }
+        
+        if (stage == 3.4 && person1.isCollidingWith(antKing)){
+            image(dialog4, 200, 500);
         }
     }
     
@@ -151,20 +165,25 @@ public class MySketch extends PApplet {
         if (stage == 2 && mouseX >= antKing.x && mouseX <= antKing.x + 100 && 
             mouseY >= antKing.y && mouseY <= antKing.y + 100) {
             stage = 3;
-            
+
             person1.x = 750; 
             person1.y = 330;
-            
+
             antKing.x = 570;
             antKing.y = 330;
         }
-        
+
         if (stage == 3 && mouseX >= 200 && mouseX <= 200 + dialog1.width && mouseY >= 500 && mouseY <= 500 + dialog1.height){
             stage = 3.1;
-        }
-        
-         if (stage == 3.1 && mouseX >= 200 && mouseX <= 200 + dialog1.width && mouseY >= 500 && mouseY <= 500 + dialog1.height){
+        }        
+        else if (stage == 3.1 && mouseX >= 200 && mouseX <= 200 + dialog2.width && mouseY >= 500 && mouseY <= 500 + dialog2.height){
             stage = 3.2;
+        }        
+        else if (stage == 3.2 && mouseX >= 200 && mouseX <= 200 + dialog3.width && mouseY >= 500 && mouseY <= 500 + dialog3.height){ // Fixed typo here (dialog3)
+            stage = 3.3;
+        }        
+        else if (stage == 3.3 && mouseX >= 0 && mouseX <= width && mouseY >= 0 && mouseY <= height){ // Fixed background click area here
+            stage = 3.4;
         }
     }
 }
